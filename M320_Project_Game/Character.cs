ae37882d92247be1c1f5c412e615ed1af3354cc3
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-12-17 08:39:14 +0100

    fix: separated class files

---
+++ M320_Project_Game/Character.cs
@@ -0,0 +1,20 @@
+﻿public class Character
+{
+    public string Name { get; }
+    protected int health;
+
+    public Character(string name, int health)
+    {
+        Name = name;
+        this.health = health;
+    }
+
+    public virtual void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Console.WriteLine($"{Name} defeated!");
+        }
+    }
+}
---
+++ M320_Project_Game/Player.cs
@@ -0,0 +1,15 @@
+﻿public class Player : Character
+{
+    private int attackPower;
+
+    public Player(string name, int health, int attackPower) : base(name, health)
+    {
+        this.attackPower = attackPower;
+    }
+
+    public void Attack(Enemy enemy)
+    {
+        Console.WriteLine($"{Name} attacks {enemy.Name}!");
+        enemy.TakeDamage(attackPower);
+    }
+}
