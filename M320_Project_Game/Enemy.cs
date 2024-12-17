8b90946db52ee70c98b754a97a10b5c2443d53b7
Pascal Bossert <Pascal_Bossert@sluz.ch>
2024-12-17 08:39:14 +0100

    feat: Implement core game structure

---
+++ M320_Project_Game/Enemy.cs
@@ -0,0 +1,10 @@
+﻿public class Enemy : Character
+{
+    public Enemy(string name, int health) : base(name, health) { }
+
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);
+        Console.WriteLine($"{Name} took {damage} damage!");
+    }
+}
---
+++ M320_Project_Game/Game.cs
@@ -0,0 +1,33 @@
+﻿public class Game
+{
+    private List<Player> players;
+    private List<Enemy> enemies;
+
+    public Game()
+    {
+        players = new List<Player>();
+        enemies = new List<Enemy>();
+    }
+
+    public void AddPlayer(Player player)
+    {
+        players.Add(player);
+    }
+
+    public void AddEnemy(Enemy enemy)
+    {
+        enemies.Add(enemy);
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Game started!");
+        foreach (var player in players)
+        {
+            foreach (var enemy in enemies)
+            {
+                player.Attack(enemy);
+            }
+        }
+    }
+}
